4b727cb7b99093661b2068017756f3deb94710fc
Sutrisno Efendi <kangfend@gmail.com>
2016-02-16 21:12:42 +0700

    Change methode name

--- toko/apps/products/tests/test_model.py
+++ toko/apps/products/tests/test_model.py
@@ -35,7 +35,7 @@ class ProductTest(TestCase):
         self.assertIn(product_2, products)
         self.assertNotIn(product_3, products)
 
-    def test_product_method(self):
+    def test_get_absolute_url(self):
         # Ensure get_absolute_url return a valid url
         self.assertEqual(self.product.get_absolute_url(),
                          '/products/%s/detail' % self.product.id)
