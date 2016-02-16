df7b2fc34592a7e1f185f5ee5eef47c332bb6110
yogi dekanata <yogi.dekanata@gmail.com>
2016-02-17 01:17:34 +0700

    resolved issue

--- toko/apps/products/admin.py
+++ toko/apps/products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Product, Photo
+from .models import Product, Photo, ProductVariation
 
 
 class PhotoInline(admin.StackedInline):
@@ -13,5 +13,10 @@ class ProductAdmin(admin.ModelAdmin):
     inlines = [PhotoInline]
 
 
+class ProductVariationAdmin(admin.ModelAdmin):
+    model = ProductVariation
+    list_display = ('name', 'is_active', 'price', 'stock')
+
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProductVariation, ProductVariationAdmin)
 
